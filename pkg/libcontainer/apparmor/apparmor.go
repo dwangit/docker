8912c955fbec4c87ce88894459939c748ff7a4a1
Michael Crosby <michael@crosbymichael.com>
2014-03-05 12:27:31 -0800

    Some cleanup around logs Docker-DCO-1.1-Signed-off-by: Michael Crosby <michael@crosbymichael.com> (github: crosbymichael)

--- pkg/libcontainer/apparmor/apparmor.go
+++ pkg/libcontainer/apparmor/apparmor.go
@@ -4,7 +4,6 @@ import (
 	"errors"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"os"
 )
 
@@ -23,20 +22,18 @@ func ApplyProfile(pid int, name string) error {
 	if !AppArmorEnabled {
 		return ErrAppArmorDisabled
 	}
+	if name == "" {
+		return nil
+	}
 
 	f, err := os.OpenFile(fmt.Sprintf("/proc/%d/attr/current", pid), os.O_WRONLY, 0)
 	if err != nil {
-		log.Printf("error open: %s\n", err)
 		return err
 	}
 	defer f.Close()
 
 	if _, err := fmt.Fprintf(f, "changeprofile %s", name); err != nil {
-		log.Printf("changeprofile %s", name)
-		log.Printf("Error write: %s\n", err)
 		return err
-	} else {
-		log.Printf("Write success!")
 	}
 	return nil
 }
